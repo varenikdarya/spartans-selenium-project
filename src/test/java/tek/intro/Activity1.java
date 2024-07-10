050a06dd8631f24aa0242b4031277971b39acbc4
darya <varenikdarya@gmail.com>
2024-07-09 21:23:43 -0400

    Navigate to Facebook Page

--- src/test/java/tek/intro/Activity1.java
+++ src/test/java/tek/intro/Activity1.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Activity1 {
     public static void main(String[] args) {
+
+     //Navigate to Facbook Page
     ChromeDriver chromeDriver = new ChromeDriver();
      chromeDriver.get("https://www.facebook.com/");
 
