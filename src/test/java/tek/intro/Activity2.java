71ade625ce12348c9fd095668f0fd5b09ccdd10c
darya <varenikdarya@gmail.com>
2024-07-09 21:55:23 -0400

    Activity 2

--- src/test/java/tek/intro/Activity2.java
+++ src/test/java/tek/intro/Activity2.java
@@ -4,13 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Activity2 {
     public static void main(String[] args) {
-        //Open Browser Chrome Browser
         ChromeDriver chromeDriver = new ChromeDriver();
-        //Navigate to URL
-        chromeDriver.get("https://dev.insurance.tekschool-students.com/");
         chromeDriver.manage().window().maximize();
+        chromeDriver.get("https://dev.insurance.tekschool-students.com/Tek Insurance UI");
         String title = chromeDriver.getTitle();
-
+        System.out.println(title);
         chromeDriver.quit();
-}
+    }
 }
