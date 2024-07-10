d651c05ef6e6619027aada42c15b20623de2823d
darya <varenikdarya@gmail.com>
2024-07-09 21:21:30 -0400

    First commit

---
+++ src/test/java/tek/intro/Activity1.java
@@ -0,0 +1,11 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+    public static void main(String[] args) {
+    ChromeDriver chromeDriver = new ChromeDriver();
+     chromeDriver.get("https://www.facebook.com/");
+
+}
+}
---
+++ src/test/java/tek/intro/HendleBrowser.java
@@ -0,0 +1,18 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HendleBrowser {
+    public static void main(String[] args) {
+
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://google.com");
+
+
+
+
+
+    }
+}
